c051f13748221647ca7362b87ce6957b5a4df4a0
Roberto Soares <roberto.tech@gmail.com>
2014-03-18 15:38:07 -0300

    added simple setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitignore ~/.gitignore
+ln -s ~/.dotfiles/.gemrc ~/.gemrc
+
+exit 0
