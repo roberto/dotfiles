188c7da613a09a3a948b9d1128d868314df4869e
Roberto Soares <rsoares@thoughtworks.com>
2019-12-07 14:14:43 -0300

    not used files

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.gitignore ~/.gitignore
-ln -s ~/.dotfiles/.gemrc ~/.gemrc
 
 exit 0
